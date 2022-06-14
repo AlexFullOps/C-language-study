2d54081432eac3502d446f86444877888aca6dce
AlexFullOps <alexfullops@gmail.com>
2022-06-14 15:41:35 -0300

    media idade

--- mediaIdade.c
+++ mediaIdade.c
@@ -6,7 +6,13 @@ int main(void)
     float media = 0.0;
 
     printf("\n Informe a idade da primeira pessoa:");
-    scanf("%d, &idade[0]");
-    
+    scanf("%d", &idade[0]);
+    printf("\n Informe a idade da segunda pessoa:");
+    scanf("%d", &idade[1]);
+    printf("\n Informe a idade da terceira pessoa:");
+    scanf("%d", &idade[2]);
+    media = (idade[0] + idade[1] + idade[2]) / 3;
+    printf("\n A media entre as 3 idades  = %.2f", media);
     return 0;
+
 }
