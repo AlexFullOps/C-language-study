c6ae687c3c9c776eb3cb5afb92aa41f0a1284072
AlexFullOps <alexfullops@gmail.com>
2022-06-14 15:31:12 -0300

    media age in C

---
+++ mediaIdade.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int idade[3] ={0, 0, 0};
+    float media = 0.0;
+
+    printf("\n Informe a idade da primeira pessoa:");
+    scanf("%d, &idade[0]");
+    
+    return 0;
+}
---
+++ test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    float altura[3] = {1, 1.5, 1.7};
+    printf("\n Vetor altura[0] = %f", altura[0]);
+    printf("\n Vetor altura[1] = %f", altura[1]);
+    printf("\n Vetor altura[2] = %f", altura[2]);
+    return 0;
+}
