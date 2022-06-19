ce5b4486fbbd2c80fd632fd7f9c22a42a1590741
AlexFullOps <alexfullops@gmail.com>
2022-06-19 14:51:21 -0300

    pre e pós incremento

---
+++ pos-incremento.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (void){
+        int x = 0;
+    int resultado = x++;
+
+    printf("\n Resultado = %d", resultado);
+printf("\n X = %d", x);
+}
---
+++ pre-incremento.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (void){
+        int x = 0;
+    int resultado = ++x;
+
+    printf("\n Rsultado = %d", resultado);
+    printf("\n X = %d", x);
+}
